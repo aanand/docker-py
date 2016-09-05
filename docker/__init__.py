93708369d1852c3ead12565c71311d110ddecd68
Ben Firshman <ben@firshman.co.uk>
2016-09-05 17:45:40 +0200

    Add start of user-focused API
    Signed-off-by: Ben Firshman <ben@firshman.co.uk>

--- docker/__init__.py
+++ docker/__init__.py
@@ -1,7 +1,7 @@
 # flake8: noqa
+from .api import APIClient, AutoVersionAPIClient
+from .client import Client, from_env
 from .version import version, version_info
 
 __version__ = version
 __title__ = 'docker-py'
-
-from .api import APIClient, AutoVersionAPIClient
---
+++ docker/client.py
@@ -0,0 +1,48 @@
+from .api.client import APIClient
+from .models.containers import ContainerCollection
+from .models.images import ImageCollection
+from .utils import kwargs_from_env
+
+
+class Client(object):
+    """
+    A client for a Docker server.
+    """
+    def __init__(self, *args, **kwargs):
+        self.api = APIClient(*args, **kwargs)
+
+    @classmethod
+    def from_env(cls, **kwargs):
+        """
+        Return a client configured with the standard set of Docker environment
+        variables. For example, `DOCKER_HOST`, `DOCKER_CERT_PATH`, etc.
+        """
+        version = kwargs.pop('version', None)
+        return cls(version=version, **kwargs_from_env(**kwargs))
+
+    # Resources
+    @property
+    def containers(self):
+        return ContainerCollection(client=self)
+
+    @property
+    def images(self):
+        return ImageCollection(client=self)
+
+    # Top-level methods
+    def events(self, *args, **kwargs):
+        return self.api.events(*args, **kwargs)
+
+    def info(self, *args, **kwargs):
+        return self.api.info(*args, **kwargs)
+
+    def login(self, *args, **kwargs):
+        return self.api.login(*args, **kwargs)
+
+    def ping(self, *args, **kwargs):
+        return self.api.ping(*args, **kwargs) == 'OK'
+
+    def version(self, *args, **kwargs):
+        return self.api.version(*args, **kwargs)
+
+from_env = Client.from_env
